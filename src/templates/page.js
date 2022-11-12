3ca09ee24d029272a19f7f55be3bd27f8311d14f
nihalDelimp <nihal@delimp.com>
2022-11-12 15:16:22 +0530

    Page Tmeplate

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,17 +1,14 @@
 import * as React from "react"
 import { graphql } from "gatsby"
 // import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
-
-
+import parse from 'html-react-parser';
 
 const Page = ({ data }) => {
   const page = data.wpPage
   return (
     <article>
       <h1>{page.title}</h1>
-      <h1>{page.content}</h1>
-      {/* <div dangerouslySetInnerHTML={{_html: page.content}}  ></div> */}
-      {/* <div>{ ReactHtmlParser(page.content) }</div> */}
+      <h1>{parse(page.content)}</h1>
       <div>{page.image}</div>
 
     </article>
