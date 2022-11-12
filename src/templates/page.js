6d12959156bd32131af60694848b71df6b7a781b
nihalDelimp <nihal@delimp.com>
2022-11-12 14:56:51 +0530

    HTml parser

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,22 +1,23 @@
 import * as React from "react"
 import { graphql } from "gatsby"
-import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
+// import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 
 
 
-const Page=({data})=>{
-    const page=data.wpPage
-    return(
-        <article>
-            <h1>{page.title}</h1>
-            {/* <div dangerouslySetInnerHTML={{_html: page.content}}  ></div> */}
-            <div>{ ReactHtmlParser(page.content) }</div>
-        <div>{page.image}</div>
-    
-        </article>
-    )
+const Page = ({ data }) => {
+  const page = data.wpPage
+  return (
+    <article>
+      <h1>{page.title}</h1>
+      <h1>{page.content}</h1>
+      {/* <div dangerouslySetInnerHTML={{_html: page.content}}  ></div> */}
+      {/* <div>{ ReactHtmlParser(page.content) }</div> */}
+      <div>{page.image}</div>
+
+    </article>
+  )
 }
-export default Page 
+export default Page
 export const query = graphql`
   query ($databaseId: Int!) {
     wpPage(databaseId: { eq: $databaseId }) {
