0783d440ee426e278535745d67c546922eb40f88
nihalDelimp <nihal@delimp.com>
2022-11-28 17:19:49 +0530

    New changessss

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,13 +12,30 @@ module.exports = {
       // "url": "https://alfanar-new.azurewebsites.net/graphql",
       // "url": "https://wordpress-gatsby.azurewebsites.net/graphql",
       // "url": "https://dev-nihal-react.pantheonsite.io/graphql",
+      // "url": "https://dev-manish-react.pantheonsite.io/graphql",
       "url": "https://alfanar-new.azurewebsites.net/graphql",
       // "url": "https://dev-alfnar-new.pantheonsite.io/graphql",
+      // "url": "https://new-wordpress-nihal.azurewebsites.net/graphql",
+      // "url": "https://new-wordpress-nihal.azurewebsites.net/graphql",
+      // "url": "https://alfanar-new.azurewebsites.net/graphql",
+      // "url": "https://alfanar-react.azurewebsites.net/graphql",
+      // "url": "https://alfnar.azurewebsites.net/graphql",
+      // "url": "https://dev-alfnar-new.pantheonsite.io/graphql",
+      // "url": "https://live-contentacms.pantheonsite.io/graphql",
       schema: {
         perPage: 2, // currently set to 100
         requestConcurrency: 5, // currently set to 15
         previewRequestConcurrency: 2, // currently set to 5
-      }
+      },
+      html: {
+        createStaticFiles: true,
+      },
+      type: {
+        MediaItem: {
+          exclude: true,
+        },
+      },
+
       // "url": "https://dev-alfanar-wp.pantheonsite.io/graphql"
     }
   }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss", "gatsby-plugin-sitemap", {
