1668ccda7e3cd7ee53b9ca05bf2423ee0ff0ed5b
Nihal Chaurasia <nihal@delimp.com>
2023-02-10 16:57:15 +0530

    Removing window href

--- src/templates/page.js
+++ src/templates/page.js
@@ -4,7 +4,6 @@ import parse from 'html-react-parser';
 import './stylestatic.css';
 const Page = ({ data }) => {
   const page = data.wpPage
-  console.log(window.location.href)
   return (
     <article>
       {page.content !== null && parse(page.content)}
